472e0de6946ece8caa779eba167363aabd3e0774
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-06-24 13:26:16 -0400

    First Commit!!!

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use axum::Router;
+use tower_http::services::ServeDir;
+
+mod db;
+
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    // ServeDir falls back to serve index.html when requesting a directory
+    let router = Router::new().nest_service("/", ServeDir::new("assets"));
+    Ok(router.into())
+}
