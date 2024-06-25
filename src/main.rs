604de634274c9b8285232cb841b49d2dbed7b9ad
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-06-25 11:32:24 -0400

    Migrate to plain Rust and Actix <- Choredom

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,141 @@
-use std::sync::Arc;
+use actix_identity::IdentityMiddleware;
+use actix_web::{cookie::Key, web, App, HttpResponse, HttpServer, Responder};
+use actix_session::SessionMiddleware;
+use actix_session_surrealdb::SurrealSessionStore;
 
-use axum::{response::Html, routing::get, Router};
-use db::{setup_db, Db};
-use surrealdb::opt::auth::Database;
-use tokio::sync::Mutex;
-use tower_http::services::ServeDir;
-
+use chrono::Duration;
 mod db;
+use db::setup_db;
 
-
-#[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
-    // ServeDir falls back to serve index.html when requesting a directory
-    let db = setup_db().await.unwrap();
-    println!("{:?}", db.version());
-    let database = Arc::new(Mutex::new(db));
-
-    let state = StateHolder{database};
-
-    let router = Router::new().nest_service("/", ServeDir::new("assets"));
-    let router = router.with_state(state);
-    
-    Ok(router.into())
+macro_rules! wapp {
+    ($e:expr; $($i:ident),+) => {
+        $e
+            $(
+                .service($i)
+            )+
+    };
 }
 
-#[derive(Clone)]
-struct StateHolder{
-    database: Arc<Mutex<db::Db>>,
+// How to do Path extractor
+// #[get("/hello/{name}")]
+// async fn greet(name: web::Path<String>) -> impl Responder {
+//     let p = format!("<p>Hello {}</p>", name);
+//     HttpResponse::Ok().body(p)
+// }
+
+// How to do Identity login 
+// #[get("/index")]
+// async fn index(user: Option<Identity>) -> impl Responder {
+//     if let Some(user) = user {
+//         format!("Welcome! {}", user.id().unwrap())
+//     } else {
+//         "Welcome Anonymous!".to_owned()
+//     }
+// }
+
+//Serve the test VVVVV
+// pub const TEST: &str = include_str!(concat!("../src-web/html/", "test", ".html"));
+// #[actix_web::get("/test")]
+// pub async fn test() -> impl actix_web::Responder{
+//     actix_web::HttpResponse::Ok().body(TEST)
+// }
+#[actix_web::get("/")]
+pub async fn homepage() -> impl Responder{
+    HttpResponse::Ok().body("Hello!")
 }
 
-// use async_trait::async_trait;
-// use serde::Serialize;
-// use shuttle_service::{resource::Type, Error, Factory, IntoResource, ResourceBuilder};
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = setup_db().await.expect("Database connection error.");
+    let app_state = web::Data::new(AppData {
+        db: Arc::new(Mutex::new(db.clone())),
+    });
 
-// #[derive(Default, Serialize)]
-// pub struct Builder {
-//     name: String,
-// }
+    // key needs to be generated outside the closure or else each worker gonna get a diff key
+    let key = Key::generate();
+    HttpServer::new(move|| {
+        wapp!(
+            App::new()
+            .app_data(web::PayloadConfig::new(20 * 1024 * 1024)) // Set limit to 10MB
+            .wrap(IdentityMiddleware::builder()
+                .visit_deadline(#[allow(clippy::unwrap_used)] Some(Duration::days(30).to_std().unwrap()))
+                .login_deadline(#[allow(clippy::unwrap_used)] Some(Duration::days(365).to_std().unwrap()))
+                .build()
+            )
+            // .wrap(SessionMiddleware::builder(
+            //     SurrealSessionStore::from_connection(db.clone(), "sessions"),
+            //     key.clone()
+            // ).build())
+            .wrap(
+                actix_web::middleware::ErrorHandlers::new()
+                .handler(actix_web::http::StatusCode::NOT_FOUND, not_found)
+            )
+            .service(actix_files::Files::new("/usr/bio", "./tmp/bio"))
+            .service(actix_files::Files::new("/usr/pfp", "./tmp/pfp"))
+            // .service(actix_files::Files::new("/tmp/chats", "./tmp/chats").show_files_listing())
+            .service(actix_files::Files::new("/src-web/assets", "./src-web/assets"))
+            .service(actix_files::Files::new("/src-web/static", "./src-web/static"));
+            homepage
+        )
+        .app_data(app_state.clone())
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
 
-// struct DatabaseOrigin{
-//     database: db::Db,
-// }
+use actix_web::{middleware::ErrorHandlerResponse, dev::ServiceResponse};
+fn not_found<B>(res: ServiceResponse<B>) -> actix_web::error::Result<ErrorHandlerResponse<B>> {
+    // split service response into request and response components
+    let (req, res) = res.into_parts();
+  
+    // set body of response to modified body
+    let res = res.set_body("NOUSER"); todo!();
+  
+    // modified bodies need to be boxed and placed in the "right" slot
+    let res = ServiceResponse::new(req, res)
+        .map_into_boxed_body()
+        .map_into_right_body();
+  
+    Ok(ErrorHandlerResponse::Response(res))
+}
 
-// impl Builder {
-//     /// Name to give resource
-//     pub fn name(mut self, name: &str) -> Self {
-//         self.name = name.to_string();
-//         self
-//     }
-// }
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use crate::db::Db;
+pub struct AppData {
+    pub db: Arc<Mutex<Db>>,
+}
+#[derive(serde::Serialize)]
+pub struct RainError{
+    message: String,
+    for_user: bool,
+}
+impl RainError{
+    // const function: fn(Self) = |x|{}; //< so weird
 
-// #[async_trait]
-// impl ResourceBuilder for Builder {
-//     const TYPE: Type = Type::Custom;
-//     type Config = Self;
-//     type Output = String;
-
-//     fn config(&self) -> &Self::Config {
-//         self
-//     }
-
-//     async fn output(self, _factory: &mut dyn Factory) -> Result<Self::Output, Error> {
-//         // factory can be used to get resources from Shuttle
-//         Ok(self.name)
-//     }
-// }
-
-// #[async_trait]
-// impl IntoResource<DatabaseOrigin> for Db {
-//     async fn into_resource(self) -> Result<DatabaseOrigin, Error> {
-//         Ok(DatabaseOrigin { database: self })
-//     }
-// }
+    pub fn from_message(message: impl ToString) -> Self{
+        let message = message.to_string();
+        // println!("{message}");
+        Self { message, for_user: false }
+    }
+    pub fn for_user(mut self) -> Self{
+        self.for_user = true;
+        self
+    }
+    pub fn to_js(self) -> HttpResponse{
+        HttpResponse::BadRequest().json(self)
+    }
+    pub fn for_js(message: impl ToString) -> HttpResponse {
+        Self::from_message(message).to_js()
+    }
+    pub fn for_js_user(message: impl ToString) -> HttpResponse {
+        Self::from_message(message).for_user().to_js()
+    }
+    pub fn for_html(message: impl ToString) -> HttpResponse{
+        HttpResponse::BadRequest().body(message.to_string())
+    }
+    // pub fn for_html_stderr() -> HttpResponse{
+    //     HttpResponse::BadRequest().body(cmd::sites::ERRHTML)
+    // }
+}
