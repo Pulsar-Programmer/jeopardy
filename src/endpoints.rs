88429259c5d8cb15d2bee3a520e767e3dcd337c3
Pulsar Programmer <95889506+Pulsar-Programmer@users.noreply.github.com>
2024-07-02 17:11:17 -0400

    Preform some House Cleaning lol

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -11,25 +11,41 @@ macro_rules! website {
 }
 website!(
     HOMEPAGE; "homepage",
-    JOIN; "join"
+    JOIN; "join",
+    PLAY; "play",
+    HOST; "host"
 );
 
 
-
-
-
-
+///This is the `index` endpoint for the browser.
 #[get("/")]
 pub async fn homepage() -> impl Responder{
     HttpResponse::Ok().body(HOMEPAGE)
 }
 
-
+///This is the `join` endpoint for the browser.
 #[get("/join")]
 pub async fn join() -> impl Responder{
     HttpResponse::Ok().body(JOIN)
 }
 
+
+
+///This is the `play` endpoint simply to display the page for the browser.
+#[get("/play")]
+pub async fn play() -> impl Responder{
+    HttpResponse::Ok().body(PLAY)
+}
+
+///This is the `host` endpoint simply to display the page for the browser.
+#[get("/host")]
+pub async fn host() -> impl Responder{
+    HttpResponse::Ok().body(HOST)
+}
+
+
+// ws://play to join as a player
+// ws://host to join as a host
 
 
 
